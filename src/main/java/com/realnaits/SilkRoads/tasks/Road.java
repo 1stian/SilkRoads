a34ab49bf3bda3ec8a67e1592e2e5b822b3954e7
Stian Tofte <stian@realnaits.com>
2020-04-03 00:39:02 +0200

    Removed unused import.

--- src/main/java/com/realnaits/SilkRoads/tasks/Road.java
+++ src/main/java/com/realnaits/SilkRoads/tasks/Road.java
@@ -4,7 +4,6 @@ import com.realnaits.SilkRoads.util.Config;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
